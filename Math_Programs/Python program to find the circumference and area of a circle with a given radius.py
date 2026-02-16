e4be3ea19eae7be6980b47412208ae5c9c4d7771
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-16 23:24:12 +0530

    Create Python program to find the circumference and area of a circle with a given radius.py

---
+++ Math_Programs/Python program to find the circumference and area of a circle with a given radius.py
@@ -0,0 +1,11 @@
+import math
+r = float(input("Input the radius of the circle: "))
+c = 2 * math.pi * r
+area = math.pi * r * r
+print("The circumference of the circle is: ", c)
+print("The area of the circle is: ", area)
+
+Output:
+Input the radius of the circle: 4
+The circumference of the circle is:  25.132741228718345
+The area of the circle is:  50.26548245743669
