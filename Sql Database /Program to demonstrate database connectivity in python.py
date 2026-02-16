f25b7cb876b73862b42424a82be7511874528d7f
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-17 00:08:58 +0530

    Create Program to demonstrate database connectivity in python.py

---
+++ Sql Database /Program to demonstrate database connectivity in python.py
@@ -0,0 +1,15 @@
+import mysql.connector
+conn = mysql.connector.connect(host='localhost', user='root', passwd='', database='db1')
+cursor = conn.cursor()
+cursor.execute("INSERT INTO books VALUES (101, 'Python')")
+conn.commit()
+print("Record inserted successfully")
+cursor.execute("SELECT * FROM books")
+result = cursor.fetchall()
+print(result)
+cursor.close()
+conn.close()
+
+O/P:
+record inserted successfully
+(101,’python’)
