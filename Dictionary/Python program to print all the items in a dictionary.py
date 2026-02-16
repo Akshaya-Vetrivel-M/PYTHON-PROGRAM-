65a9e4871ffba465c62c8dcd25686af019958c4c
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-16 23:53:26 +0530

    Create Python program to print all the items in a dictionary.py

---
+++ Dictionary/Python program to print all the items in a dictionary.py
@@ -0,0 +1,14 @@
+phone_book = {
+    'Arun': ['9876543210', 'arun@gmail.com'],        # boy
+    'Senthil': ['9123456789', 'senthil@gmail.com'], # boy
+    'Kavitha': ['9001122334', 'kavitha@gmail.com'], # girl
+    'Selvi': ['9345678901', 'selvi@gmail.com']      # girl
+}
+for k, v in phone_book.items():
+    print(k, ":", v)
+
+O/P:
+Arun : ['9876543210', 'arun@gmail.com']
+Sarvesh : ['9123456789', 'sarvesh@gmail.com']
+Akshaya : ['9001122334', 'akshaya@gmail.com']
+Selva : ['9345678901', 'selva@gmail.com']
