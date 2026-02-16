b0032517d1314f3067b3ae159241197c828dc505
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-17 00:07:43 +0530

    Create Program to find the value of one number raised to the power of another.py

---
+++ Math_Programs/Program to find the value of one number raised to the power of another.py
@@ -0,0 +1,9 @@
+m=int(input('Enter First value: '))
+n =int(input('Enter Second value: '))
+p=m**n
+print('{0} Raise to {1} = {2}'.format(m,n,p))
+
+O/P:
+Enter First value: 10
+Enter Second value: 12
+10 Raise to 12 = 1000000000000
