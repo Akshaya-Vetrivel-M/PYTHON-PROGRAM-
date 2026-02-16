ea1de29ee39da1316412a3770d7279db255949fa
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-16 23:29:31 +0530

    Create  Python program to find the geometric mean of n numbers .py

---
+++ Conditionals/Python program to find the geometric mean of n numbers .py
@@ -0,0 +1,15 @@
+c = 0
+p = 1.0
+count = int(input("Enter the number of values: "))
+while(c<count):
+    x = float(input("Enter a real number: "))
+    c = c+1
+    p = p * x
+gm = pow(p,1.0/count)
+print("The geometric mean is: ",gm)
+
+Output:
+Enter the number of values: 2
+Enter a real number: 12
+Enter a real number: 25
+The geometric mean is:  17.320508075688775
