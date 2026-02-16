c98a35ead0978f10e694dfde19f00277ecb0e56d
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-17 00:00:19 +0530

    Create Python program to Toggle Characters Case in a String using swapcase().py

---
+++ Strings/Python program to Toggle Characters Case in a String using swapcase().py
@@ -0,0 +1,9 @@
+string = input("Please Enter your Own String : ")
+string1 = string.swapcase()
+print("\nOriginal String=  ", string)
+print("The Given String After Toggling Case =  ", string1)
+
+O/P:
+Please Enter your Own String : Akshaya
+Original String=   Akshaya
+The Given String After Toggling Case =   aKSHAYA
