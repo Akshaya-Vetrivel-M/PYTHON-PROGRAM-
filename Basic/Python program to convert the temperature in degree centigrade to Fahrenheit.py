e03706ba6f2d020bb05c957cc8b01af79b180515
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-16 23:19:41 +0530

    Create Python program to convert the temperature in degree centigrade to Fahrenheit.py

---
+++ Basic/Python program to convert the temperature in degree centigrade to Fahrenheit.py
@@ -0,0 +1,7 @@
+c=input("Enter the Temperature in Centigrade:")
+f=(9*(int(c))/5)+32
+print ("Temperature in Fahrenheit is:",f)
+
+Output:
+Enter the Temperature in Centigrade:45
+Temperature in Fahrenheit is: 113.0
