87af378acf206694d3c1c734e3d03249f6f36beb
Akshaya-Vetrivel-M <akshaya.vetrivel5@gmail.com>
2026-02-16 23:24:55 +0530

    Create Python program to check whether the given integer is a multiple of 5.py

---
+++ Math_Programs/Python program to check whether the given integer is a multiple of 5.py
@@ -0,0 +1,9 @@
+number = int(input("Enter an integer: "))
+if(number%5==0):
+    print(number, "is a multile of 5")
+else:
+    print(number, "is not a multiple of 5")
+
+Output:
+Enter an integer: 25
+25 is a multiple of 5
